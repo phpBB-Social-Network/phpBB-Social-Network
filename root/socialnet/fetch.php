bf74e95055cd6201be3a6c3079dab269acb4865e
Culprit-CZ <jankalach@gmail.com>
2012-08-28 11:19:48 +0200

    UPDATE 0.7.0 RC1

--- root/socialnet/fetch.php
+++ root/socialnet/fetch.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package phpBB Social Network
- * @version 0.6.3
+ * @version 0.7.0
  * @copyright (c) phpBB Social Network Team 2010-2012 http://phpbbsocialnetwork.com
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  *
