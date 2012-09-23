eab77fbd89b3930c16b51d1fc7c79180706fe1ab
Senky <jakubsenko@gmail.com>
2012-09-23 16:33:47 +0200

    finalising issue2pr.php

--- git-tools/issue2pr.php
+++ git-tools/issue2pr.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ *
+ * @package phpBB Social Network
+ * @version 0.7.0
+ * @copyright (c) phpBB Social Network Team 2010-2012 http://phpbbsocialnetwork.com
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+/*
+ * This file helps you to change issue on github to pull request.
+ * It is not possible to merge issue and pull request already created,
+ * so you need to push your changes to your fork and specify all data
+ * needed to create new pull request. Note that:
+ *    - branch name is fetched from local repo and its branch currently
+ *      set, so if you want to create pull request from another branch,
+ *      you need to specify it.
+ *    - pull request will be created always against "develop" branch.
+ */
 
 $username = $_GET['username'];
 $password = $_GET['password'];
@@ -41,24 +60,28 @@ if ( isset($_GET['username'], $_GET['password'], $_GET['issue_id'], $_GET['branc
 			Github username: <input type="text" name="username" placeholder="Github username" value="<?php echo `git config user.name`; ?>" /><br />
 			Github password: <input type="password" name="password" placeholder="Github password" /><br />
 			Issue ID: <input type="text" name="issue_id" placeholder="Issue ID" /><br />
-			Branch: <input type="text" name="branch" placeholder="Branch" value="<?php echo get_branch_name(); ?>" />
+			Branch: <input type="text" name="branch" placeholder="Branch" value="<?php echo get_branch_name(); ?>" /><br />
+			<input type="submit" />
 		</form>
 
 	</body>
 </html>
 <?php
 
-function do_post_request($url, $data, $optional_headers = null)
+/**
+ * sends POST request
+ *
+ * @param  string $url		url to connect
+ * @param  string $data		POST data
+ *
+ * @return mixed					response from remote url
+ */
+function do_post_request($url, $data)
 {
 	$params = array('http' => array(
 		'method' => 'POST',
 		'content' => $data
 	));
-
-	if ($optional_headers !== null)
-	{
-		$params['http']['header'] = $optional_headers;
-	}
 
 	$ctx = stream_context_create($params);
 	$fp = @fopen($url, 'rb', false, $ctx);
@@ -78,6 +101,11 @@ function do_post_request($url, $data, $optional_headers = null)
 	return $response;
 }
 
+/**
+ * gets branch user is currently on
+ *
+ * @return string		name of branch user is currently on
+ */
 function get_branch_name()
 {
 	$head = file_get_contents('../.git/HEAD');
