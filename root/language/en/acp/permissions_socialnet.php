c2acf0256f88c67e99c57172a52c3dcb0dbea083
Senky <jakubsenko@gmail.com>
2012-10-28 12:24:19 +0100

    [i/49] rewording acl_u_sn_notify

--- root/language/en/acp/permissions_socialnet.php
+++ root/language/en/acp/permissions_socialnet.php
@@ -28,7 +28,7 @@ $lang['permission_cat']['socialnet'] = 'Social Network';
 $lang = array_merge($lang, array(
 	'acl_a_sn_settings'			 => array('lang' => 'Can edit Social Network settings', 'cat' => 'settings'),
 	'acl_u_sn_im'				 => array('lang' => 'Can use Instant Messenger', 'cat' => 'socialnet'),
-	'acl_u_sn_notify'			 => array('lang' => 'Can use Notify', 'cat' => 'socialnet'),
+	'acl_u_sn_notify'			 => array('lang' => 'Can use notifications', 'cat' => 'socialnet'),
 	'acl_u_sn_userstatus'		 => array('lang' => 'Can use User status', 'cat' => 'socialnet'),
 	'acl_m_sn_close_reports'	 => array('lang' => 'Can close user reports', 'cat' => 'misc'),
 ));
