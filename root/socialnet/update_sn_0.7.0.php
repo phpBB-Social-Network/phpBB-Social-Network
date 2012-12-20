be278c1c0d4c80cf8834c0152a7d9791e62fd104
Kamahl19 <kamahl19@gmail.com>
2012-12-20 22:51:03 +0100

    add http_build_query()

--- root/socialnet/update_sn_0.7.0.php
+++ root/socialnet/update_sn_0.7.0.php
@@ -1101,4 +1101,40 @@ function sql_rename_table($new_name)
 	}
 }
 
+if ( !function_exists('http_build_query'))
+{
+	function http_build_query($data, $prefix = '', $separator = arg_separator.output, $enc_type = PHP_QUERY_RFC1738)
+	{
+	    $queryString = '';
+
+	    if (is_array($data))
+	    {
+	        foreach ($data as $key => $value)
+	        {
+	            $correctKey = $prefix;
+
+	            if ('' === $prefix)
+	            {
+	                $correctKey .= $key;
+	            }
+	            else
+	            {
+	                $correctKey .= "[" . $key . "]";
+	            }
+
+	            if (!is_array($value))
+	            {
+	                $queryString .= urlencode($correctKey) . "=" . urlencode($value) . $separator;
+	            }
+	            else
+	            {
+	                $queryString .= http_build_query($value, $correctKey, $separator, $enc_type);
+	            }
+	        }
+	    }
+
+		return substr($queryString, 0, strlen($queryString) - 1);
+	}
+}
+
 ?>
